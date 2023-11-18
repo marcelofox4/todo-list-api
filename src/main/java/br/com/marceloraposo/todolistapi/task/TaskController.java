14219ade0437db1fe1bb1cfa03873b5c5cf24885
marcelofox4 <marcelofox4@hotmail.com>
2023-11-17 22:11:13 -0300

    verificação de datas na criação de uma task

--- src/main/java/br/com/marceloraposo/todolistapi/task/TaskController.java
+++ src/main/java/br/com/marceloraposo/todolistapi/task/TaskController.java
@@ -1,18 +1,20 @@
 package br.com.marceloraposo.todolistapi.task;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.var;
 
 @RestController
 @RequestMapping("/task")
@@ -41,4 +43,10 @@ public class TaskController {
         return ResponseEntity.status(HttpStatus.OK).body(task);
     }
 
+    @GetMapping("/")
+    public List<TaskModel> list(HttpServletRequest request) {
+        var idUser = request.getAttribute("idUser");
+        var tasks = this.taskRepository.findByIdUser((UUID) idUser);
+        return tasks;
+    }
 }
